48be99f8865c9e7d78dc90c98e4351a686910241
CallumHoughton18 <callum.houghton13@hotmail.co.uk>
2021-12-21 17:35:29 +0000

    Add initial POF snow storm for the christmas period

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,2 +1,18 @@
 import "./src/scss/index.scss";
 import "./src/scss/globals.scss";
+
+import snowStorm from "./src/libs/snowStorm.js";
+
+export const onInitialClientRender = () => {
+
+    const currentDate = new Date();
+    const currentYear = currentDate.getFullYear();
+    const beginningFestiveSeason = new Date(currentYear, 11, 19)
+    const endFestiveSeason = new Date(currentYear + 1, 0, 6)
+
+    currentDate.setDate(currentDate.getDate() + 1)
+    const inRange = currentDate >= beginningFestiveSeason && currentDate < endFestiveSeason;
+    if (inRange) {
+        snowStorm()
+    }
+};
