d746102f767c95fb2d39c0d5aff5ef12ae0ec30c
CallumHoughton18 <callum.houghton13@hotmail.co.uk>
2021-12-22 17:03:07 +0000

    Refactor snowStorm to TypeScript implementation, add festive utility function to check date

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,18 +1,11 @@
 import "./src/scss/index.scss";
 import "./src/scss/globals.scss";
 
-import snowStorm from "./src/libs/snowStorm.js";
+import { snowStorm, isInFestiveSeason } from "./src/easter_eggs/snowStorm";
 
 export const onInitialClientRender = () => {
-
     const currentDate = new Date();
-    const currentYear = currentDate.getFullYear();
-    const beginningFestiveSeason = new Date(currentYear, 11, 19)
-    const endFestiveSeason = new Date(currentYear + 1, 0, 6)
-
-    currentDate.setDate(currentDate.getDate() + 1)
-    const inRange = currentDate >= beginningFestiveSeason && currentDate < endFestiveSeason;
-    if (inRange) {
+    if (isInFestiveSeason(currentDate)) {
         snowStorm()
     }
 };
--- src/easter_eggs/snowStorm.ts
+++ src/easter_eggs/snowStorm.ts
@@ -1,30 +1,38 @@
-export default () => {
+type SnowFlake = {
+  x: number;
+  y: number;
+  opacity: number;
+  speedX: number;
+  speedY: number;
+  radius: number;
+};
+
+export const snowStorm = (): void => {
   const canvas = document.createElement("canvas");
   canvas.style.position = "fixed";
-  canvas.style.top = 0;
-  canvas.style.left = 0;
-  canvas.style.zIndex = -1;
+  canvas.style.top = "0";
+  canvas.style.left = "0";
+  canvas.style.zIndex = "-1";
   canvas.id = "snow";
   document.body.appendChild(canvas);
   const ctx = canvas.getContext("2d");
   const particlesOnScreen = 100;
-  const particlesArray = [];
-  let w, h;
+  const particlesArray: SnowFlake[] = [];
+  let w: number, h: number;
   w = canvas.width = window.innerWidth;
   h = canvas.height = window.innerHeight;
 
-  function random(min, max) {
+  function random(min: number, max: number) {
     return min + Math.random() * (max - min + 1);
-  };
+  }
 
-  function clientResize(ev) {
+  function clientResize() {
     w = canvas.width = window.innerWidth;
     h = canvas.height = window.innerHeight;
-  };
+  }
   window.addEventListener("resize", clientResize);
 
   function createSnowFlakes() {
-    console.log("creating snw flakes...")
     for (let i = 0; i < particlesOnScreen; i++) {
       particlesArray.push({
         x: Math.random() * w,
@@ -33,24 +41,33 @@ export default () => {
         speedX: random(-11, 11),
         speedY: random(7, 15),
         radius: random(0.5, 4.2),
-      })
+      });
     }
-  };
+  }
 
   function drawSnowFlakes() {
     for (let i = 0; i < particlesArray.length; i++) {
-      var gradient = ctx.createRadialGradient(
+      const gradient = ctx.createRadialGradient(
         particlesArray[i].x,
         particlesArray[i].y,
         0,
         particlesArray[i].x,
         particlesArray[i].y,
-        particlesArray[i].radius
+        particlesArray[i].radius,
       );
 
-      gradient.addColorStop(0, "rgba(255, 255, 255," + particlesArray[i].opacity + ")");  // white
-      gradient.addColorStop(.8, "rgba(210, 236, 242," + particlesArray[i].opacity + ")");  // bluish
-      gradient.addColorStop(1, "rgba(237, 247, 249," + particlesArray[i].opacity + ")");   // lighter bluish
+      gradient.addColorStop(
+        0,
+        "rgba(255, 255, 255," + particlesArray[i].opacity + ")",
+      ); // white
+      gradient.addColorStop(
+        0.8,
+        "rgba(210, 236, 242," + particlesArray[i].opacity + ")",
+      ); // bluish
+      gradient.addColorStop(
+        1,
+        "rgba(237, 247, 249," + particlesArray[i].opacity + ")",
+      ); // lighter bluish
 
       ctx.beginPath();
       ctx.arc(
@@ -59,13 +76,13 @@ export default () => {
         particlesArray[i].radius,
         0,
         Math.PI * 2,
-        false
+        false,
       );
 
       ctx.fillStyle = gradient;
       ctx.fill();
     }
-  };
+  }
 
   function moveSnowFlakes() {
     for (let i = 0; i < particlesArray.length; i++) {
@@ -77,14 +94,24 @@ export default () => {
         particlesArray[i].y = -50;
       }
     }
-  };
+  }
 
   function updateSnowFall() {
     ctx.clearRect(0, 0, w, h);
     drawSnowFlakes();
     moveSnowFlakes();
-  };
+  }
 
   setInterval(updateSnowFall, 50);
   createSnowFlakes();
+};
+
+export const isInFestiveSeason = (givenDate: Date): boolean => {
+  const dateCopy = new Date(givenDate.getTime());
+  const givenYear = dateCopy.getFullYear();
+  const beginningFestiveSeason = new Date(givenYear, 11, 19);
+  const endFestiveSeason = new Date(givenYear + 1, 0, 6);
+
+  givenDate.setDate(dateCopy.getDate() + 1);
+  return dateCopy >= beginningFestiveSeason && dateCopy < endFestiveSeason;
 };
