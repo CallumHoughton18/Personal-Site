f47d82d9f18fd6c03b912e0404cc7af4860f90f5
CallumHoughton18 <callum.houghton13@hotmail.co.uk>
2022-05-04 21:27:34 +0100

    reorder blog posts

--- src/pages/blog.tsx
+++ src/pages/blog.tsx
@@ -6,19 +6,17 @@ import PostSummary from "@/components/page components/blog/PostSummary";
 
 const Blog = (): JSX.Element => {
   const posts = useBlogPostsQuery();
-  const postSummaries = posts.blog.posts
-    .map(post => (
-      <PostSummary
-        key={post.id}
-        title={post.frontmatter.title}
-        author={post.frontmatter.author}
-        date={post.frontmatter.date}
-        excerpt={post.excerpt}
-        postExternalLink={post.frontmatter.externalUrl}
-        postInternalLink={post.fields.slug}
-      />
-    ))
-    .reverse();
+  const postSummaries = posts.blog.posts.map(post => (
+    <PostSummary
+      key={post.id}
+      title={post.frontmatter.title}
+      author={post.frontmatter.author}
+      date={post.frontmatter.date}
+      excerpt={post.excerpt}
+      postExternalLink={post.frontmatter.externalUrl}
+      postInternalLink={post.fields.slug}
+    />
+  ));
   return (
     <PageWithNavBar header={<Header />}>
       <div className="flex flex-col justify-center items-center mr-5 ml-5">
