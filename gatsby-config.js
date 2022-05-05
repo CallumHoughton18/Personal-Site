b10bdc36f19326fe8c206ffccccfe05d5197544e
CallumHoughton18 <callum.houghton13@hotmail.co.uk>
2022-05-05 14:51:52 +0100

    add blog feed config to gatsby-config

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,26 @@
+function fixRelativeLinks({ html, siteUrl }) {
+  // fix static links
+  html = html.replace(
+    /(?<=\"|\s)\/static\//g,
+    `${siteUrl}\/static\/`,
+  );
+
+  // fix relative links
+  html = html.replace(/(?<=href=\")\//g, `${siteUrl}\/`);
+
+  return html;
+}
+
 module.exports = {
   // Since `gatsby-plugin-typescript` is automatically included in Gatsby you
   // don't need to define it here (just if you need to change the options)
   siteMetadata: {
-    title: 'Gatsby Default Starter',
+    title: 'Callums Site',
+    description: 'Portfolio + Blog Posts',
+    author: {
+      name: 'Callum'
+    },
+    siteUrl: 'https://site.callums-stuff.net',
     menuLinks: [
       {
         name: 'home',
@@ -65,6 +83,59 @@ module.exports = {
           }, 
         ], 
       }, 
+    },
+    {
+      resolve: `gatsby-plugin-feed`,
+      options: {
+        query: `
+          {
+            site {
+              siteMetadata {
+                title
+                description
+                siteUrl
+                site_url: siteUrl
+              }
+            }
+          }
+        `,
+        feeds: [
+          {
+            serialize: ({ query: { site, allMarkdownRemark } }) => {
+              return allMarkdownRemark.edges.map(edge => {
+                return Object.assign({}, edge.node.frontmatter, {
+                  description: edge.node.excerpt,
+                  date: edge.node.frontmatter.date,
+                  url: site.siteMetadata.siteUrl + edge.node.fields.slug,
+                  guid: site.siteMetadata.siteUrl + edge.node.fields.slug,
+                  custom_elements: [{ "content:encoded": edge.node.html }],
+                })
+              })
+            },
+            query: `
+              {
+                allMarkdownRemark(
+                  sort: { order: DESC, fields: [frontmatter___date] },
+                ) {
+                  edges {
+                    node {
+                      excerpt
+                      html
+                      fields { slug }
+                      frontmatter {
+                        title
+                        date
+                      }
+                    }
+                  }
+                }
+              }
+            `,
+            output: "/rss.xml",
+            title: "Callum Houghtons Blog Feed",
+          },
+        ],
+      },
     },
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,   
